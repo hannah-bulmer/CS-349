5ea882d9478099be0a26b191abbba7a268a6b587
Hannah Bulmer <hannah.bulmer@elastic.io>
2021-05-28 18:05:40 -0400

    Process args

--- a1/Rename.java
+++ a1/Rename.java
@@ -2,39 +2,86 @@ import java.io.File;
 import java.lang.Runtime;
 import java.text.DecimalFormat;
 import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Rename {
 
-  static DecimalFormat formatter = new DecimalFormat("###,###,###,###");
+    public static HashMap<String,ArrayList<String>> map=new HashMap<String,ArrayList<String>>();
+    public static HashSet<String> set = new HashSet<>(Arrays.asList(
+            "-h", "-help", "-f", "-file", "-p", "-prefix", "-s", "-suffix", "-r", "-replace"));
 
-  static final String ANSI_RESET = "\u001B[0m";
-  static final String ANSI_BLACK = "\u001B[30m";
-  static final String ANSI_RED = "\u001B[31m";
-  static final String ANSI_GREEN = "\u001B[32m";
-  static final String ANSI_YELLOW = "\u001B[33m";
-  static final String ANSI_BLUE = "\u001B[34m";
-  static final String ANSI_PURPLE = "\u001B[35m";
-  static final String ANSI_CYAN = "\u001B[36m";
-  static final String ANSI_WHITE = "\u001B[37m";
+    public static void main(String[] args) {
+        Rename runner = new Rename();
 
-  public static void main(String[] args) {
-    new Rename();
-  }
+        runner.processArgs(args);
 
-  Rename() {
-    try {
-
-      System.out.println();
-      System.out.println("  ;)(;  " + " Vendor:  " + System.getProperty("java.vendor"));
-      System.out.println(" :----: " + " JDK:     " + System.getProperty("java.vm.name"));
-      System.out.println("C|====| " + " Version: " + System.getProperty("java.version"));
-      System.out.println(" |    | " + " Memory:  " + Runtime.getRuntime().totalMemory()/(1024*1024) + " MB");
-      System.out.println(" `----' " + " Free:    " + Runtime.getRuntime().freeMemory()/(1024*1024) + " MB");
-      System.out.println();
-
-    } catch (Exception ex) {
-      System.out.println(ex.toString());
+        System.out.println(Arrays.asList(map)); // method 1
     }
-  }
+
+    public static void returnError(String err) {
+        System.out.println(err);
+        System.exit(0);
+    }
+
+    public static void printHelp() {
+        System.out.println("I am the help menu!");
+        System.exit(0);
+    }
+
+    Rename() {}
+
+    public void processArgs(String[] args) {
+        int len = args.length;
+        if (len == 0) returnError("Error, please add at least one flag");
+
+        String curFlag = "";
+        for (int i = 0; i < len; i ++) {
+            String arg = args[i];
+            if (arg.charAt(0) == '-') {
+                if (set.contains(arg)) {
+                    String key = arg.substring(0,2);
+                    System.out.println("Adding " + key);
+                    curFlag = key;
+                    if (map.containsKey(key)) returnError("Error, flag " + key + " given more than once");
+                    map.put(key, new ArrayList<>());
+                }
+            } else {
+                // argument
+                if (curFlag.equals("")) returnError("Error, please add at least one flag");
+                if (curFlag.equals("-h")) returnError("Error, -help does not take any arguments");
+                ArrayList<String> arguments = map.get(curFlag);
+                if (curFlag.equals("-r") && arguments.size() == 2)
+                    returnError("Error, -replace only takes 2 arguments");
+
+                // convert @date and @time into the date/time
+                arguments.add(arg);
+            }
+        }
+
+        // print help menu
+        if (map.containsKey("-h")) printHelp();
+
+        // error checking
+
+        // need at least 1 file provided
+        if (!map.containsKey("-f")) returnError("Error, please use -f option to add a file");
+        if (map.get("-f").size() == 0) returnError("Error, please specify at least one file");
+        // need at least 1 of replace, suffix, prefix
+        if (!map.containsKey("-r") && !map.containsKey("-p") && !map.containsKey("-s"))
+            returnError("Error, please add some options for how you would like to rename your files");
+
+        // if replace, need exactly 2 args
+        if (map.containsKey("-r") && map.get("-r").size() != 2)
+            returnError("Error using -r, please specify exactly two values");
+
+        // if p or s, need at least 1 arg
+        if (map.containsKey("-p") && map.get("-p").size() == 0)
+            returnError("Error using -p, please specify at least one prefix");
+        if (map.containsKey("-s") && map.get("-s").size() == 0)
+            returnError("Error using -s, please specify at least one suffix");
+    }
 }
 
