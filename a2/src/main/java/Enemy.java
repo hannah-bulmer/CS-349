4915e483b5f419118b4f9cb11207691a16f1f130
Hannah Bulmer <hannah.bulmer@elastic.io>
2021-06-12 17:55:04 -0400

    Moving aliens

--- a2/src/main/java/Enemy.java
+++ a2/src/main/java/Enemy.java
@@ -1,2 +1,66 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Enemy {
+    Image image;
+    ImageView imageView;
+    int size = 40;
+
+    int CHANGE = 2;
+
+    /**
+     * @param path
+     * @param x
+     * @param y
+     * @throws FileNotFoundException
+     */
+    public Enemy(String path, float x, float y) throws FileNotFoundException {
+        image = new Image(new FileInputStream(path));
+        imageView = new ImageView(image);
+        imageView.setX(x);
+        imageView.setY(y);
+        imageView.setFitHeight(size);
+        imageView.setFitWidth(size);
+    }
+
+    /**
+     * @param path
+     * @throws FileNotFoundException
+     */
+    public Enemy(String path) throws FileNotFoundException {
+        image = new Image(new FileInputStream(path));
+        imageView = new ImageView(image);
+        imageView.setFitHeight(size);
+        imageView.setFitWidth(size);
+    }
+
+    public ImageView getEnemy() {
+        return imageView;
+    }
+
+    /**
+     * @param x
+     */
+    public void setCenterX(float x) {
+        imageView.setX(x);
+    }
+
+    /**
+     * @param y:
+     */
+    public void setCenterY(float y) {
+        imageView.setY(y);
+    }
+
+    public void handle_animation(float left, float right) {
+        float x = (float)(imageView.getX());
+        if (x < left) CHANGE *= -1;
+        if (x > right) CHANGE *= -1;
+        this.setCenterX(x + CHANGE);
+    }
 }
--- a2/src/main/java/SpaceInvaders.java
+++ a2/src/main/java/SpaceInvaders.java
@@ -5,21 +5,21 @@ import javafx.scene.Group;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.scene.paint.*;
-import javafx.scene.shape.*;
+import javafx.scene.paint.Color;
 
 public class SpaceInvaders extends Application {
     float screen_width = 800;
     float screen_height = 600;
     float left_edge = 20;
-    float right_edge = screen_width - left_edge;
+    float right_edge = screen_width - 3 * left_edge;
 
-    int num_balls = 8;
-    Circle [] balls = new Circle [num_balls];
-    float change = 5;
+    int num_balls = 10;
+    int rows = 5;
+    Enemy [][] balls = new Enemy [rows][num_balls];
     float left_ball_x = left_edge + 15;
-    float ball_dist_apart = 60;
-    float ball_y = 50;
+    float ball_x_dist = 60;
+    float ball_y = 25;
+    float ball_y_dist = 50;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -27,13 +27,17 @@ public class SpaceInvaders extends Application {
         stage.setResizable(false);
         stage.setTitle("Space Invaders");
 
-        float loc = left_ball_x;
-        for (int i = 0; i < 8; i ++) {
-            balls[i] = new Circle(15);
-            balls[i].setCenterX(loc);
-            balls[i].setCenterY(ball_y);
-            balls[i].setFill(Color.RED);
-            loc += ball_dist_apart;
+        float x = left_ball_x;
+        float y = ball_y;
+        for (int i = 0; i < rows; i ++) {
+            for (int j = 0; j < num_balls; j ++) {
+                balls[i][j] = new Enemy("src/resources/images/enemy1.png");
+                balls[i][j].setCenterX(x);
+                balls[i][j].setCenterY(y);
+                x += ball_x_dist;
+            }
+            x = left_ball_x;
+            y += ball_y_dist;
         }
 
         AnimationTimer timer = new AnimationTimer() {
@@ -47,8 +51,10 @@ public class SpaceInvaders extends Application {
 
         Group root = new Group();
 
-        for (Circle ball:balls) {
-            root.getChildren().add(ball);
+        for (Enemy[] row:balls) {
+            for (Enemy e: row) {
+                root.getChildren().add(e.getEnemy());
+            }
         }
 
         Label label = new Label("Hello World!!");
@@ -58,19 +64,17 @@ public class SpaceInvaders extends Application {
         Scene level3 = new Scene(new StackPane(), screen_width, screen_height);
 
 
+        level1.setFill(Color.BLACK);
         stage.setScene(level1);
         stage.show();
     }
 
     void handle_animation() {
-        if (left_ball_x < left_edge + 15) change *= -1;
-        if (left_ball_x + (num_balls-1) * ball_dist_apart > right_edge-15) change *= -1;
-        left_ball_x += change;
-        float tmp = left_ball_x;
-        for (Circle ball: balls) {
-            System.out.println(tmp);
-            ball.setCenterX(tmp);
-            tmp += ball_dist_apart;
+        for (int i = 0; i < rows; i ++) {
+            for (int j = 0; j < num_balls; j ++) {
+                balls[i][j].handle_animation(left_edge + j * ball_x_dist,
+                        right_edge - (num_balls - j - 1) * ball_x_dist);
+            }
         }
     }
 }
